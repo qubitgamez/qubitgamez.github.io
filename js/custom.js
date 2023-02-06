633d099304ca14a2f4a211f4d179c068f36fe69e
krishnachaitanya <chaitanya415@gmail.com>
2023-02-07 02:30:21 +0530

    loading gif and other fixes

--- js/custom.js
+++ js/custom.js
@@ -1193,8 +1193,8 @@ var Industry = function(){
 	}
 	
 	var handleSupport = function(){
-		var support = '<a href="https://1.envato.market/1d1Nm" target="_blank" class="bt-buy-now theme-btn"><i class="ti-shopping-cart"></i><span>Buy Now</span></a><a href="https://support.w3itexperts.com" target="_blank" class="bt-support-now theme-btn"><i class="ti-headphone-alt"></i><span>Support</span></a><!-- Go to www.addthis.com/dashboard to customize your tools --><script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b221c5e31b4e54b"></script>';
-		jQuery('body').append(support);
+		// var support = '<a href="https://1.envato.market/1d1Nm" target="_blank" class="bt-buy-now theme-btn"><i class="ti-shopping-cart"></i><span>Buy Now</span></a><a href="https://support.w3itexperts.com" target="_blank" class="bt-support-now theme-btn"><i class="ti-headphone-alt"></i><span>Support</span></a><!-- Go to www.addthis.com/dashboard to customize your tools --><script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b221c5e31b4e54b"></script>';
+		// jQuery('body').append(support);
 	}
 	
 	/* Function ============ */
